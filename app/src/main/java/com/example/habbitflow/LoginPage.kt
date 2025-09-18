0c6248553e71d058dfbd271116b9173481c76a74
Lawa-777 <lawanyanisal@gmail.com>
2025-09-18 19:02:40 +0530

    login was designed

--- app/src/main/java/com/example/habbitflow/LoginPage.kt
+++ app/src/main/java/com/example/habbitflow/LoginPage.kt
@@ -1,27 +1,20 @@
 package com.example.habbitflow
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class OnBoardingScreen2 : AppCompatActivity() {
+class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_on_boarding_screen2)
+        setContentView(R.layout.activity_login_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val onboarding1 = findViewById<Button>(R.id.next)
-        onboarding1.setOnClickListener {
-            val intent = Intent(this, OnBoardingScreen3::class.java)
-            startActivity(intent)
         }
     }
 }
